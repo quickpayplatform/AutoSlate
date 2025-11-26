b3b0c0937cabb4d2f19230013e0fff0077d787ca
quickpayplatform <Quickpayplatform@gmail.com>
2025-11-25 23:18:22 -0500

    Initial Commit

---
+++ SkipSlate/SkipSlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkipSlateApp.swift
+//  SkipSlate
+//
+//  Created by Tee Forest on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct SkipSlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkipSlateTests/SkipSlateTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkipSlateTests.swift
+//  SkipSlateTests
+//
+//  Created by Tee Forest on 11/25/25.
+//
+
+import Testing
+@testable import SkipSlate
+
+struct SkipSlateTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
